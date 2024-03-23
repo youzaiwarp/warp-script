c2edf1cc1a6b6748d5a8f477a8cabc057559117a
youzaiwarp <162280354+youzaiwarp@users.noreply.github.com>
2024-03-24 05:54:55 +0800

    Add files via upload

--- files/warp-yxip/warp-yxip-mac.sh
+++ files/warp-yxip/warp-yxip-mac.sh
@@ -17,16 +17,7 @@ yellow() {
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
-for i in "${CMD[@]}"; do
-    SYS="$i" && [[ -n $SYS ]] && break
-done
-
-for ((int = 0; int < ${#REGEX[@]}; int++)); do
-    if [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]]; then
-        SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
-    fi
-done
-
+# 选择客户端 CPU 架构
 archAffix(){
     case "$(uname -m)" in
         x86_64 | amd64 ) echo 'amd64' ;;
@@ -35,177 +26,48 @@ archAffix(){
     esac
 }
 
-endpointyx(){
-    # 下载优选工具软件，感谢悠哉免费分享电报TG群组：https://t.me/youzaiYYDS
-    wget https://mirror.ghproxy.com/https://raw.githubusercontent.com/youzaiwarp/warp-script/main/files/warp-yxip/warp-darwin-$(archAffix) -O warp
+endpointyx(){    
+    # 删除之前的优选结果文件，以避免出错
+    rm -f result.csv
+
+    # 下载优选工具软件，感谢 GitHub 项目：https://github.com/peanut996/CloudflareWarpSpeedTest
+    wget https://gitlab.com/Misaka-blog/warp-script/-/raw/main/files/warp-yxip/warp-darwin-$(archAffix) -O warp
+    
+    # 取消 Linux 自带的线程限制，以便生成优选 Endpoint IP
+    ulimit -n 102400
     
     # 启动 WARP Endpoint IP 优选工具
-    chmod +x warp && ./warp >/dev/null 2>&1
+    chmod +x warp
+
+    if [[ $1 == 6 ]]; then
+        ./warp -ipv6
+    else
+        ./warp
+    fi
     
     # 显示前十个优选 Endpoint IP 及使用方法
-    green "当前最优 Endpoint IP 结果如下，并已保存至 result.csv中："
+    green "当前最优 Endpoint IP 结果如下，并已保存至 result.csv 中："
     cat result.csv | awk -F, '$3!="timeout ms" {print} ' | sort -t, -nk2 -nk3 | uniq | head -11 | awk -F, '{print "端点 "$1" 丢包率 "$2" 平均延迟 "$3}'
     echo ""
-    yellow "使用方法如下："
-    yellow "1. 将 WireGuard 节点的默认的 Endpoint IP：engage.cloudflareclient.com:2408 替换成本地网络最优的 Endpoint IP"
+    yellow "优选 IP 使用方法如下："
+    yellow "1. 将 WARP 的 WireGuard 节点的默认的 Endpoint IP：engage.cloudflareclient.com:2408 替换成本地网络最优的 Endpoint IP"
+    yellow "使用教程：https://blog.misaka.rest/2023/01/25/wireguard-warp"
 
     # 删除 WARP Endpoint IP 优选工具及其附属文件
-    rm -f warp ip.txt
-}
-
-endpoint4(){
-    # 生成优选 WARP IPv4 Endpoint IP 段列表
-    n=0
-    iplist=100
-    while true; do
-        temp[$n]=$(echo 162.159.192.$(($RANDOM % 256)))
-        n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-        temp[$n]=$(echo 162.159.193.$(($RANDOM % 256)))
-        n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-        temp[$n]=$(echo 162.159.195.$(($RANDOM % 256)))
-        n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-        temp[$n]=$(echo 162.159.204.$(($RANDOM % 256)))
-        n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-        temp[$n]=$(echo 188.114.96.$(($RANDOM % 256)))
-        n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-        temp[$n]=$(echo 188.114.97.$(($RANDOM % 256)))
-        n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-        temp[$n]=$(echo 188.114.98.$(($RANDOM % 256)))
-        n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-        temp[$n]=$(echo 188.114.99.$(($RANDOM % 256)))
-        n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-    done
-    while true; do
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo 162.159.192.$(($RANDOM % 256)))
-            n=$(($n + 1))
-        fi
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo 162.159.193.$(($RANDOM % 256)))
-            n=$(($n + 1))
-        fi
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo 162.159.195.$(($RANDOM % 256)))
-            n=$(($n + 1))
-        fi
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo 162.159.204.$(($RANDOM % 256)))
-            n=$(($n + 1))
-        fi
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo 188.114.96.$(($RANDOM % 256)))
-            n=$(($n + 1))
-        fi
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo 188.114.97.$(($RANDOM % 256)))
-            n=$(($n + 1))
-        fi
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo 188.114.98.$(($RANDOM % 256)))
-            n=$(($n + 1))
-        fi
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo 188.114.99.$(($RANDOM % 256)))
-            n=$(($n + 1))
-        fi
-    done
-
-    # 将生成的 IP 段列表放到 ip.txt 里，待程序优选
-    echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u > ip.txt
-
-    # 启动优选程序
-    endpointyx
-}
-
-endpoint6(){
-    # 生成优选 WARP IPv6 Endpoint IP 段列表
-    n=0
-    iplist=100
-    while true; do
-        temp[$n]=$(echo [2606:4700:d0::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
-        n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-        temp[$n]=$(echo [2606:4700:d1::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
-        n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-    done
-    while true; do
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo [2606:4700:d0::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
-            n=$(($n + 1))
-        fi
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo [2606:4700:d1::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
-            n=$(($n + 1))
-        fi
-    done
-
-    # 将生成的 IP 段列表放到 ip.txt 里，待程序优选
-    echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u > ip.txt
-
-    # 启动优选程序
-    endpointyx
+    rm -f warp
 }
 
 menu(){
     clear
     echo "#############################################################"
-    echo -e "# ${RED}WARP Endpoint IP 悠哉一键优选脚本${PLAIN}        #"
-    echo -e "# ${GREEN}电报TG群组${PLAIN}: 综合资源交流分享             #"
-    echo -e "# ${GREEN}群组${PLAIN}: https://t.me/youzaiYYDS            #"
-    echo -e "# ${GREEN}频道${PLAIN}: https://t.me/youzaiV587            #"
-    echo -e "# ${GREEN}免费${PLAIN}:                                    #"
-    echo -e "# ${GREEN}分享${PLAIN}:                                    #"
-    echo -e "# ${GREEN}交流${PLAIN}:                                    #"
-    echo -e "# ${GREEN}悠哉${PLAIN}:                                    #"
+    echo -e "#               ${RED}WARP Endpoint IP 一键优选脚本${PLAIN}               #"
+    echo -e "# ${GREEN}作者${PLAIN}: MisakaNo の 小破站                                  #"
+    echo -e "# ${GREEN}博客${PLAIN}: https://blog.misaka.rest                            #"
+    echo -e "# ${GREEN}GitHub 项目${PLAIN}: https://github.com/Misaka-blog               #"
+    echo -e "# ${GREEN}GitLab 项目${PLAIN}: https://gitlab.com/Misaka-blog               #"
+    echo -e "# ${GREEN}Telegram 频道${PLAIN}: https://t.me/misakanocchannel              #"
+    echo -e "# ${GREEN}Telegram 群组${PLAIN}: https://t.me/misakanoc                     #"
+    echo -e "# ${GREEN}YouTube 频道${PLAIN}: https://www.youtube.com/@misaka-blog        #"
     echo "#############################################################"
     echo ""
     echo -e " ${GREEN}1.${PLAIN} WARP IPv4 Endpoint IP 优选 ${YELLOW}(默认)${PLAIN}"
@@ -213,11 +75,11 @@ menu(){
     echo " -------------"
     echo -e " ${GREEN}0.${PLAIN} 退出脚本"
     echo ""
-    read -rp "悠哉提示请输入选项 [0-2]: " menuInput
+    read -rp "请输入选项 [0-2]: " menuInput
     case $menuInput in
-        2 ) endpoint6 ;;
+        2 ) endpointyx 6 ;;
         0 ) exit 1 ;;
-        * ) endpoint4 ;;
+        * ) endpointyx ;;
     esac
 }
 
